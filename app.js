6041dbf12e59e6a3883986163b245fc4ba923e37
Avinash372 <avinash.sahu3123@gmail.com>
2022-08-28 13:34:00 +0530

    created the DB and Schema for Posts

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
+let Post = require("./models/post.model").Post;
+
+mongoose.connect("mongodb://localhost/travels");
+
+let post1 = new Post({
+    id: "2",
+    title: "Statue of Liberty",
+    date: new Date(),
+    description: "some description",
+    text: "some text",
+    country: "USA",
+    ImageURL: "/images/img-2",
+});
+
+post1.save().then(() => console.log("saved..."));
 
 app.use(express.static("public"));
 
